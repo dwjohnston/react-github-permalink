05152d8ecc3f249d197208ae6558706aa308ffe8
David Johnston <david@blacksheepcode.com>
2023-11-15 16:23:52 +1100

    Add a prepublishOnly script. Remove dist files from version control.

--- dist/GithubPermalink.d.ts
+++
@@ -1,6 +0,0 @@
-type GithubPermalinkProps = {
-    permalink: string;
-    className?: string;
-};
-export declare function GithubPermalink(props: GithubPermalinkProps): import("react/jsx-runtime").JSX.Element | null;
-export {};
