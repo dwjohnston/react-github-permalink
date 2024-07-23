b11524350351ad241e003044ad4659f94d81feed
David Johnston <david.johnston@coles.com.au>
2024-07-23 16:29:55 +1000

    Upgrade to Storybook 8

--- src/GithubIssueLink/GithubIssueLink.stories.tsx
+++ src/GithubIssueLink/GithubIssueLink.stories.tsx
@@ -1,8 +1,11 @@
-import type { Meta, StoryObj  } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { GithubIssueLink,} from './GithubIssueLink';
-import { GithubPermalinkContext, GithubPermalinkProvider} from "../config/GithubPermalinkContext";
-import "../GithubPermalink/github-permalink.css"
+import { GithubIssueLink } from "./GithubIssueLink";
+import {
+  GithubPermalinkContext,
+  GithubPermalinkProvider,
+} from "../config/GithubPermalinkContext";
+import "../GithubPermalink/github-permalink.css";
 
 const meta: Meta<typeof GithubIssueLink> = {
   component: GithubIssueLink,
@@ -18,16 +21,24 @@ type Story = StoryObj<typeof GithubIssueLink>;
  * to learn how to use render functions.
  */
 export const Primary: Story = {
-  render: () => <GithubIssueLink issueLink='https://github.com/dwjohnston/react-github-permalink/issues/2' />,
+  render: () => (
+    <GithubIssueLink issueLink="https://github.com/dwjohnston/react-github-permalink/issues/2" />
+  ),
 };
 
 export const WithBackground: Story = {
-  render: () => <div style={{backgroundColor: "pink", padding: "1em"}}><GithubIssueLink issueLink='https://github.com/dwjohnston/react-github-permalink/issues/2' /></div>,
+  render: () => (
+    <div style={{ backgroundColor: "pink", padding: "1em" }}>
+      <GithubIssueLink issueLink="https://github.com/dwjohnston/react-github-permalink/issues/2" />
+    </div>
+  ),
 };
 
-
-
 export const WithToken: Story = {
-  render: () => <GithubPermalinkProvider githubToken={process.env.STORYBOOK_GITHUB_TOKEN}> <GithubIssueLink issueLink='https://github.com/dwjohnston/react-github-permalink/issues/2' /></GithubPermalinkProvider>,
-
-}
+  render: () => (
+    <GithubPermalinkProvider githubToken={process.env.STORYBOOK_GITHUB_TOKEN}>
+      {" "}
+      <GithubIssueLink issueLink="https://github.com/dwjohnston/react-github-permalink/issues/2" />
+    </GithubPermalinkProvider>
+  ),
+};
