7028f697e359cfcabd2ace2fbbf2fcd66dfe9b34
David Johnston <david.johnston@coles.com.au>
2024-08-06 17:46:22 +1000

    Remove theme prop

--- src/library/GithubPermalink/GithubPermalinkRsc.tsx
+++ src/library/GithubPermalink/GithubPermalinkRsc.tsx
@@ -2,15 +2,10 @@
 import { githubPermalinkRscConfig } from "../config/GithubPermalinkRscConfig"
 import { GithubPermalinkBase } from "./GithubPermalinkBase";
 
-
-
-
 export type GithubPermalinkRscProps = {
     className?: string;
     permalink: string;
 }
-
-
 
 export async function GithubPermalinkRsc(props: GithubPermalinkRscProps) {
     const dataFn = githubPermalinkRscConfig.getPermalinkFn();
@@ -18,8 +13,5 @@ export async function GithubPermalinkRsc(props: GithubPermalinkRscProps) {
     const onError = githubPermalinkRscConfig.getOnError();
 
     const data = await dataFn(props.permalink, token, onError);
-
-    return <GithubPermalinkBase permalink={props.permalink} data={data} className={props.className} isDarkMode={true} />
-
-
+    return <GithubPermalinkBase permalink={props.permalink} data={data} className={props.className}/>
 }
