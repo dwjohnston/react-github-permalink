4bdfce5bc6abf3c86435fa7a7144c6c745ee93c0
David Johnston <david.johnston@coles.com.au>
2024-08-06 17:42:55 +1000

    Splat - switch to NextJS as a the compiler

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { GithubPermalinkRsc } from "../library/GithubPermalink/GithubPermalinkRsc";
+import ReactSyntaxHighlighter from "react-syntax-highlighter";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+
+        <ReactSyntaxHighlighter>
+          asd
+          asdasd
+          asd
+        </ReactSyntaxHighlighter>
+
+        <GithubPermalinkRsc permalink="https://github.com/dwjohnston/react-github-permalink/blob/5b15aa07e60af4e317086f391b28cadf9aae8e1b/sample_files/sample1.go#L1-L5" />
+
+
+      </div>
+    </main>
+  );
+}
