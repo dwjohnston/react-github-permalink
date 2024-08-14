1717dd0ac4a441abc599fcbfa0c5bb2d5886befc
David Johnston <david@blacksheepcode.com>
2024-08-14 17:28:52 +1000

    Update exports

--- src/library/export.ts
+++ src/library/export.ts
@@ -1,6 +1,5 @@
+"use client"
 export * from "./GithubPermalink/GithubPermalink";
 export * from "./GithubPermalink/GithubPermalinkBase";
-export * from "./GithubPermalink/GithubPermalinkRsc";
 export * from "./GithubIssueLink/GithubIssueLink";
 export * from "./config/GithubPermalinkContext";
-export * from "./config/GithubPermalinkRscConfig";
