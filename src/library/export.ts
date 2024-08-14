2fe06dcd9e527f22a973833224384998745cc646
David Johnston <david@blacksheepcode.com>
2024-08-14 20:19:01 +1000

    Add IssueLInk to the exports. Update docs

--- src/library/export.ts
+++ src/library/export.ts
@@ -2,4 +2,5 @@
 export * from "./GithubPermalink/GithubPermalink";
 export * from "./GithubPermalink/GithubPermalinkBase";
 export * from "./GithubIssueLink/GithubIssueLink";
+export * from "./GithubIssueLink/GithubIssueLinkBase"
 export * from "./config/GithubPermalinkContext";
