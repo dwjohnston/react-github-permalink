13606069cf58bc8b0e9e2d7795e6894c3c7e3078
David Johnston <david@blacksheepcode.com>
2024-08-20 16:01:49 +1000

    Fix status colors

--- src/library/GithubIssueLink/GithubIssueLinkBase.tsx
+++ src/library/GithubIssueLink/GithubIssueLinkBase.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren } from "react";
 import { GithubSvg } from "../GithubSvg/GithubSvg";
 import {   GithubIssueLinkDataResponse } from "../config/GithubPermalinkContext";
 import { ErrorMessages } from "../ErrorMessages/ErrorMessages";
+import { Reactions } from "../common/Reactions/Reactions";
 
 
 type GithubIssueLinkBaseProps = {
@@ -10,6 +11,9 @@ type GithubIssueLinkBaseProps = {
     issueLink: string;
     data: GithubIssueLinkDataResponse;
 }
+
+
+
 export function GithubIssueLinkBase(props: GithubIssueLinkBaseProps) {
     const {data} =  props; 
 
@@ -30,7 +34,12 @@ export function GithubIssueLinkBase(props: GithubIssueLinkBaseProps) {
             </svg><span> Open</span></div> : <div className="react-github-issuelink-status closed"><svg aria-hidden="true" height="12" viewBox="0 0 16 16" version="1.1" width="12" data-view-component="true" >
               <path d="M11.28 6.78a.75.75 0 0 0-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 0 0-1.06 1.06l2 2a.75.75 0 0 0 1.06 0l3.5-3.5Z"></path><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1.5 0a6.5 6.5 0 1 0-13 0 6.5 6.5 0 0 0 13 0Z"></path>
             </svg><span> Closed</span></div>}
+
+       
           </div>
+          <div>
+                {data.reactions && <Reactions reactions={data.reactions}/>}
+            </div>
         </>}>
         </GithubIssueLinkInner>
     
