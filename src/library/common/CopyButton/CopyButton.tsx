22c8d7f08c8df4798876158cf22fb584ab035e45
David Johnston <david@blacksheepcode.com>
2024-10-10 16:57:29 +1100

    Adds tooltip to copy button.  (#41)
    * Initial add tooltips
    
    * All updates for copy tooltip.
    
    * Fix icon style.
    
    * 1.10.1

--- src/library/common/CopyButton/CopyButton.tsx
+++ src/library/common/CopyButton/CopyButton.tsx
@@ -1,13 +1,72 @@
 "use client"
 
+import { useEffect, useId, useRef, useState } from "react";
 import { CopySvg } from "../../images/CopySvg";
+import { TickSvg } from "../../images/TickSvg";
 
 
 export function CopyButton(props: { clipboard: string }) {
     const { clipboard } = props;
-    return <button title="Copy snippet" onClick={() => {
-        navigator.clipboard.writeText(clipboard)
+
+    const idCopy = useId();
+    const idCopied = useId();
+
+    const copyRef = useRef<HTMLDivElement>(null);
+    const copiedRef = useRef<HTMLDivElement>(null);
+
+    const [isClicked, setIsClicked] = useState(false);
+
+
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsClicked(false);
+            copiedRef.current?.hidePopover();
+        }, 2000)
+
+        return () => {
+            clearTimeout(timeout);
+
+            
+        }
+    }, [isClicked])
+
+
+    return <div className="tooltip-container">
+
+         {/* @ts-ignore */}
+        <div className="tooltip-target" id={idCopy} popover="auto" ref={copyRef}></div>
+        <div className="tooltip-content" >Copy snippet</div>
+        {/* @ts-ignore */}
+        <div className="tooltip-target" id={idCopied} popover="auto" ref={copiedRef}></div>
+        <div className="tooltip-content">Copied!</div>
+        <button 
+        className={isClicked? "clicked": "not-clicked"}
+        onMouseEnter={() =>{
+
+            if(!isClicked){
+                copyRef.current?.showPopover();
+            }
+        }}
+
+        onMouseLeave={(()=>{
+            copyRef.current?.hidePopover()
+        })}
+
+        // Need to use React 19 to get the proper typings :/
+        // @ts-ignore- https://github.com/facebook/react/issues/27479
+        popoverTarget={idCopied}
+        title="Copy snippet" 
+        onClick={() => {
+        navigator.clipboard.writeText(clipboard);
+        setIsClicked(true);
+
+
     }}>
-        <CopySvg />
+
+        {isClicked ? <TickSvg/>:
+        <CopySvg />}
     </button>
+    </div>
+
 }
