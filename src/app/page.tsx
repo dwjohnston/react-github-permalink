6368f1ee0a4dd2d49735fa9056eeeea5a55d0180
David Johnston <david@blacksheepcode.com>
2025-06-27 14:08:57 +1000

    Fix the build command and add info the site page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-
-        <ReactSyntaxHighlighter>
-          asd
-          asdasd
-          asd
-        </ReactSyntaxHighlighter>
-
-        <GithubPermalinkRsc permalink="https://github.com/dwjohnston/react-github-permalink/blob/5b15aa07e60af4e317086f391b28cadf9aae8e1b/sample_files/sample1.go#L1-L5" />
+        <h1>React Github Permalink</h1>
 
 
+        See the <a href="/storybook-static/index.html">Storybook</a>
       </div>
     </main>
   );
