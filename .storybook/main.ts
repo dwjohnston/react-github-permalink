aa9c7d2bde86f9dfe748bcc58a5917536bffb108
David Johnston <david@blacksheepcode.com>
2025-06-27 14:56:35 +1000

    Get rid of some stuff

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,5 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
 
-const config: StorybookConfig = {
+const config = {
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { GithubPermalinkRsc } from "../library/GithubPermalink/GithubPermalinkRsc";
-import ReactSyntaxHighlighter from "react-syntax-highlighter";
-import "../library/GithubPermalink/github-permalink.css"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div >
         <h1>React Github Permalink</h1>
 
 
