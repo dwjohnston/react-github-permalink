0e7d5bb4db728a208eb93a4d5e9236e3c54192c5
David Johnston <david@blacksheepcode.com>
2025-06-27 14:43:26 +1000

    Update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,9 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding", 
+    "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
+    "@storybook/addon-docs"
   ],
   framework: "@storybook/nextjs",
   "staticDirs": [
