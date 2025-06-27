2264cb4063957fb4195acd6c76bca9ee26d7f662
David Johnston <david@blacksheepcode.com>
2025-06-27 13:58:18 +1000

    Fix lint error (#63)
    * Fix lint error
    
    * Fix config import

--- src/library/ErrorMessages/ErrorMessages.tsx
+++ src/library/ErrorMessages/ErrorMessages.tsx
@@ -2,40 +2,40 @@ import { ErrorResponses } from "../config/GithubPermalinkContext";
 import { exhaustiveFailure } from "../utils/exhaustiveFailure";
 
 export function ErrorMessages(props: {
-    data: ErrorResponses
+  data: ErrorResponses
 }) {
 
-    const data = props.data;    
-    if(data.status === "404"){
-        return <>
-          <p className="error"> 
-           Github returned an HTTP status 404. Is this a private Github repo? 
-          </p>
-        </>
-      }
-    
-      if(data.status ==="rate-limit"){
-        return <>
-          <p className="error"> 
-          You have encountered Github's unauthenticated API request rate limit. You can still visit the above link to see the code snippet.
-          </p>
-        </>
-      }
-      if(data.status === "unauthorized"){
-        return <>
-        <p className="error"> 
-          Unauthorized. Is your Github token valid?
-        </p>
-      </>
-      }
-    
-      if(data.status ==="other-error"){
-        return <>
-          <p className="error"> 
-              An unknown error occurred.
-          </p>
-        </>
-      }
+  const data = props.data;
+  if (data.status === "404") {
+    return <>
+      <p className="error">
+        Github returned an HTTP status 404. Is this a private Github repo?
+      </p>
+    </>
+  }
 
-      return exhaustiveFailure(data);
+  if (data.status === "rate-limit") {
+    return <>
+      <p className="error">
+        You have encountered Github&#39;s unauthenticated API request rate limit. You can still visit the above link to see the code snippet.
+      </p>
+    </>
+  }
+  if (data.status === "unauthorized") {
+    return <>
+      <p className="error">
+        Unauthorized. Is your Github token valid?
+      </p>
+    </>
+  }
+
+  if (data.status === "other-error") {
+    return <>
+      <p className="error">
+        An unknown error occurred.
+      </p>
+    </>
+  }
+
+  return exhaustiveFailure(data);
 }
